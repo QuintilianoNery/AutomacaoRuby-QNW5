450487692ce0a612f83266d6dc70df0312fa6aee
Quintiliano Paquini Nery <quintiliano1995@gmail.com>
2021-06-06 20:55:52 -0300

    aula 9

--- parodify/features/step_definitions/cadastro_steps.rb
+++ parodify/features/step_definitions/cadastro_steps.rb
@@ -1,11 +1,10 @@
 Dado('que acesso a página de cadastro') do       
     visit "http://parodify.qaninja.com.br"
-    sleep 5
 end                                                                           
                                                                                 
-  Quando('submeto o meu cadastro com:') do |table|                              
-    # table is a Cucumber::MultilineArgument::DataTable                         
-    pending # Write code here that turns the phrase above into concrete actions 
+  Quando('submeto o meu cadastro com:') do |table|     
+    #converte a tabela que está entre pipe para um objeto nativo do ruby                  
+    log table.rows_hash
 end                                                                           
                                                                                 
   Então('devo ser redirecionado para a área logada') do                         
