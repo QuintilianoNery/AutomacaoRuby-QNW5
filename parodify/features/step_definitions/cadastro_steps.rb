98c971afc247fdb79bace5365628b23445a18718
Quintiliano <qpnery@paschoalotto.com.br>
2021-06-15 07:15:01 -0300

    Identação Rufo

--- parodify/features/step_definitions/cadastro_steps.rb
+++ parodify/features/step_definitions/cadastro_steps.rb
@@ -1,39 +1,39 @@
 #@happy
-Dado('que acesso a página de cadastro:') do       
-    visit "http://parodify.qaninja.com.br"
-    click_on "EXPERIMENTE AGORA"
-end                                                                           
-                                                                                
-Quando('submeto o meu cadastro com:') do |table|     
-    #converte a tabela que está entre pipe para um objeto nativo do ruby              
-    #guardar na variável user    
-    user = table.rows_hash
+Dado("que acesso a página de cadastro:") do
+  visit "http://parodify.qaninja.com.br"
+  click_on "EXPERIMENTE AGORA"
+end
 
-    #usar API para apagar o cadastro antes de ser utilizado
-    apagar_cadastro user[:email]
-    
-    #find método do capybara para buscar elemento
-    #preenchendo o campo e-mail
-    find("input[id='user_email']").set user[:email]
+Quando("submeto o meu cadastro com:") do |table|
+  #converte a tabela que está entre pipe para um objeto nativo do ruby
+  #guardar na variável user
+  user = table.rows_hash
 
-    #preencher o campo senha
-    find("input[id='user_password']").set user[:senha]
+  #usar API para apagar o cadastro antes de ser utilizado
+  apagar_cadastro user[:email]
 
-    #preencher o campo confirmar senha
-    find("input[id='user_password_confirmation']").set user[:senha_confirma]
+  #find método do capybara para buscar elemento
+  #preenchendo o campo e-mail
+  find("input[id='user_email']").set user[:email]
 
-    #clicar no botão confirmar
-    click_on "Cadastrar"
-end                                                                           
-                                                                                
-  Então('devo ser redirecionado para a área logada') do
-    #validação com rspec
-    #a página deve conter o seletor css .dashboard
-    expect(page).to have_css '.dashboard'
-end                                                                           
+  #preencher o campo senha
+  find("input[id='user_password']").set user[:senha]
+
+  #preencher o campo confirmar senha
+  find("input[id='user_password_confirmation']").set user[:senha_confirma]
+
+  #clicar no botão confirmar
+  click_on "Cadastrar"
+end
+
+Então("devo ser redirecionado para a área logada") do
+  #validação com rspec
+  #a página deve conter o seletor css .dashboard
+  expect(page).to have_css ".dashboard"
+end
 
 #Aproveitamento do código para ser usado em todos os cenários
-Então('devo ver a mensagem {string}') do |expect_message|
+Então("devo ver a mensagem {string}") do |expect_message|
   alert = find(".message p")
   expect(alert.text).to eql expect_message
 end
--- parodify/features/support/helpers.rb
+++ parodify/features/support/helpers.rb
@@ -2,8 +2,7 @@ require "HTTParty"
 
 #Função que chama a api de exclusão do cadastro que usa determinado e-mail
 module Helpers
-    
-    def apagar_cadastro(email)
-        HTTParty.get("http://parodify.qaninja.com.br/helpers?email=#{email}")
-    end
+  def apagar_cadastro(email)
+    HTTParty.get("http://parodify.qaninja.com.br/helpers?email=#{email}")
+  end
 end
